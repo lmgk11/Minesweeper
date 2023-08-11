23f4225706cf76ea48fa19507c4a8c66e848a898
Lucas Gillberg Kling <lulle.gillbergkling@gmail.com>
2023-08-11 04:17:51 +0200

    general rendering setup is done

--- tile.py
+++ tile.py
@@ -2,6 +2,7 @@ class Tile:
     def __init__(self, bomb):
         self.bomb = bomb # bool to set bombs
         self.number = None
+        self.revealed = False
     def set_number(self, number):
         self.number = number
     def __str__(self):
