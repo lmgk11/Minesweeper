3c90faf8c3f7c4edda0c32f7473a1710e5e531b7
Lucas Gillberg Kling <lulle.gillbergkling@gmail.com>
2023-08-11 07:49:18 +0200

    now basically a fully playable minesweeper, left to implement is the bombs not being able to be on the starting square and then the AI

--- game.py
+++ game.py
@@ -10,12 +10,11 @@ class Game:
         for i in range(self.ROWS):
             col = []
             for j in range(self.COLS):
-                col.append(tile.Tile(True if random.random() > 0.8 else False))
+                col.append(tile.Tile(True if random.random() > 0.4 else False))
                 
             self.tiles.append(col)
 
         self.assign_tile_number()
-        self.debug_print()
 
     def assign_tile_number(self):
         for i in range(self.ROWS):
@@ -26,22 +25,52 @@ class Game:
                     self.tiles[i][j].number = 0
                     if i + 1 < self.ROWS and self.tiles[i+1][j].bomb:
                         self.tiles[i][j].number += 1
-                    if i - 1 > 0 and self.tiles[i-1][j].bomb:
+                    if i - 1 >= 0 and self.tiles[i-1][j].bomb:
                         self.tiles[i][j].number += 1
                     if j + 1 < self.COLS and self.tiles[i][j+1].bomb:
                         self.tiles[i][j].number += 1
-                    if j - 1 > 0 and self.tiles[i][j-1].bomb:
+                    if j - 1 >= 0 and self.tiles[i][j-1].bomb:
                         self.tiles[i][j].number += 1
                     if i + 1 < self.ROWS and j + 1 < self.COLS and self.tiles[i+1][j+1].bomb:
                         self.tiles[i][j].number += 1
-                    if i - 1 > 0 and j - 1 > 0 and self.tiles[i-1][j-1].bomb:
+                    if i - 1 >= 0 and j - 1 >= 0 and self.tiles[i-1][j-1].bomb:
                         self.tiles[i][j].number += 1
-                    if i + 1 < self.ROWS and j - 1 > 0 and self.tiles[i+1][j-1].bomb:
+                    if i + 1 < self.ROWS and j - 1 >= 0 and self.tiles[i+1][j-1].bomb:
                         self.tiles[i][j].number += 1
-                    if i - 1 > 0 and j + 1 < self.COLS and self.tiles[i-1][j+1].bomb:
+                    if i - 1 >= 0 and j + 1 < self.COLS and self.tiles[i-1][j+1].bomb:
                         self.tiles[i][j].number += 1
     def debug_print(self):
         for i in range(self.ROWS):
             for j in range(self.COLS):
                 print(f' {self.tiles[i][j]} ', end='')
             print()
+    def reveal(self, row, col):
+        if 0 <= row < self.ROWS and 0 <= col < self.COLS and not self.tiles[row][col].revealed:
+            self.tiles[row][col].revealed = True
+
+            if self.tiles[row][col].number == 0:
+                self.reveal(row + 1, col)
+                self.reveal(row - 1, col)
+                self.reveal(row, col + 1)
+                self.reveal(row, col - 1)
+                self.reveal(row + 1, col + 1)
+                self.reveal(row + 1, col - 1)
+                self.reveal(row - 1, col - 1)
+                self.reveal(row - 1, col + 1)
+    def flag(self, row, col):
+        if 0 <= row < self.ROWS and 0 <= col < self.COLS and not self.tiles[row][col].revealed:
+            self.tiles[row][col].flagged = not self.tiles[row][col].flagged
+    def game_status(self):
+        won = True
+        for i in range(self.ROWS):
+            for j in range(self.COLS):
+                if self.tiles[i][j].bomb and self.tiles[i][j].revealed:
+                    return [False, False]
+                if not self.tiles[i][j].revealed and not self.tiles[i][j].bomb:
+                    won = False
+        if won:
+            return [False, True]
+        else:
+            return [True, None]
+
+                
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 import game
+import easygui
 
-CELL_SIZE, HEIGHT, WIDTH = 60, 10, 10
+CELL_SIZE, HEIGHT, WIDTH = 60, 15, 20
 
 pygame.init()
 
@@ -12,10 +13,13 @@ clock = pygame.time.Clock()
 
 pygame.display.set_caption("Physics")
 
-font = pygame.font.Font('assets/fonts/mine-sweeper.ttf', CELL_SIZE - 15)
+font = pygame.font.Font('assets/fonts/mine-sweeper.ttf', CELL_SIZE - 20)
 
 g = game.Game(WIDTH, HEIGHT)
-#print(g.tiles)
+
+flag_image = pygame.transform.scale(pygame.image.load('assets/images/flag.png'), (round(CELL_SIZE*0.6), round(CELL_SIZE*0.6)))
+running = True
+first_game = True
 
 def draw_board():
     for row in range(HEIGHT):
@@ -23,21 +27,87 @@ def draw_board():
             # draw the grid
             pygame.draw.rect(screen, (0, 0, 0), (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE), 1)
             #check if it is revealed
-            if g.tiles[row][col].revealed:
-                pygame.draw.rect(screen, (128, 128, 128), (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
-            else: 
+            if not g.tiles[row][col].revealed:
+                if g.tiles[row][col].flagged:
+                    pygame.draw.rect(screen, (128, 128, 128), (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+                    number_surface = font.render(chr(0x0060), True, (92, 0, 0))
+                    number_rect = number_surface.get_rect(center=(col * CELL_SIZE + CELL_SIZE // 2, row * CELL_SIZE + CELL_SIZE // 2))
+                    screen.blit(number_surface, number_rect)
+                    
+                    pygame.draw.rect(screen, (60, 60, 60), (col * CELL_SIZE + 1, row * CELL_SIZE + 1, CELL_SIZE - 2, CELL_SIZE - 2), 2)
+                    pygame.draw.rect(screen, (100, 100, 100), (col * CELL_SIZE + 3, row * CELL_SIZE + 3, CELL_SIZE - 6, CELL_SIZE - 6), 3)
+                    
+                else:
+                    pygame.draw.rect(screen, (128, 128, 128), (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+                    pygame.draw.rect(screen, (60, 60, 60), (col * CELL_SIZE + 1, row * CELL_SIZE + 1, CELL_SIZE - 2, CELL_SIZE - 2), 2)
+                    pygame.draw.rect(screen, (100, 100, 100), (col * CELL_SIZE + 3, row * CELL_SIZE + 3, CELL_SIZE - 6, CELL_SIZE - 6), 3)
+
+            elif g.tiles[row][col].number != 0: 
                 pygame.draw.rect(screen, (192, 192, 192), (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
                 number_surface = font.render(f'{g.tiles[row][col]}', True, (0, 0, 0))
                 number_rect = number_surface.get_rect(center=(col * CELL_SIZE + CELL_SIZE // 2, row * CELL_SIZE + CELL_SIZE // 2))
                 screen.blit(number_surface, number_rect)
+                pygame.draw.rect(screen, (0, 0, 0), (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE), 2)
+            else:
+                pygame.draw.rect(screen, (192, 192, 192), (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+                pygame.draw.rect(screen, (0, 0, 0), (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE), 2)
             pygame.draw.rect(screen, (0, 0, 0), (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE), 1)
 
-while True:
+def display_msg(msg):
+    
+    msg_text = font.render(msg, True, (0, 0, 0))
+    msg_rect = msg_text.get_rect(center=(CELL_SIZE * (WIDTH) // 2, CELL_SIZE * (HEIGHT) // 2))
+    screen.blit(msg_text, msg_rect)
+
+    continue_button_text = font.render('Continue', True, (0, 92, 0))
+    continue_button = continue_button_text.get_rect(center=(CELL_SIZE * (WIDTH) // 2, CELL_SIZE * (HEIGHT + 2) // 2))
+    screen.blit(continue_button_text, continue_button)
+
+    quit_button_text = font.render('Quit', True, (92, 0, 0))
+    quit_button = quit_button_text.get_rect(center=(CELL_SIZE * (WIDTH) // 2, CELL_SIZE * (HEIGHT + 4) // 2))
+    screen.blit(quit_button_text, quit_button)
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if continue_button.collidepoint(event.pos[0], event.pos[1]):
+                return False
+            if quit_button.collidepoint(event.pos[0], event.pos[1]):
+                pygame.quit()
+                exit()
+            
+    return True
+
+
+
+while running:
     screen.fill((255, 255, 255))
-    draw_board()
+    
+    if first_game:
+        first_game = display_msg('welcome')
+    else:
+        status = g.game_status()
+        if not status[0]:
+            replay = not display_msg('You Win!' if status[1] else 'Game Over!')
+            if replay:
+                g = game.Game(WIDTH, HEIGHT)
+                status[0] = True
+        else:
+            draw_board()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                left_click, middle_click, right_click = pygame.mouse.get_pressed()
+                if left_click:
+                    g.reveal(event.pos[1] // CELL_SIZE, event.pos[0] // CELL_SIZE)
+                elif right_click:
+                    g.flag(event.pos[1] // CELL_SIZE, event.pos[0] // CELL_SIZE)
+    
+        
+            
+
+    
+    
     pygame.display.flip()
 
